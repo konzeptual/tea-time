871e52e08cbe887a14fdc6371acd64c3cc392263
Gabriel Saldana <gabriel@justia.com>
2011-04-15 15:24:04 -0500

    Added contact email to copyright notice

--- tea-time.el
+++ tea-time.el
@@ -3,7 +3,7 @@
 ;; Author: konsty <antipin.konstantin@googlemail.com>
 ;; Keywords: timer tea-time
 
-;; Copyright (C) 2011 Gabriel Saldana
+;; Copyright (C) 2011 Gabriel Saldana <gsaldana@gmail.com>
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
