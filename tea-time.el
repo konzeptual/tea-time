0ee528343008061f4157ff8344051113d0408e74
Samuel W. Flint <swflint@flintfam.org>
2016-09-04 20:57:38 -0500

    Reformated code and made a little bit lisp-ier

--- tea-time.el
+++ tea-time.el
@@ -77,27 +77,23 @@
   "sound that will play once timer is expired.
 If you don't have alsa, it is better to be .wav file"
   :group 'tea-time
-  :type 'string
-  )
+  :type 'string)
 
 (defcustom tea-time-sound-command nil
   "Command to run to play sounds."
   :group 'tea-time
-  :type 'string
-)
+  :type 'string)
 
 (defvar tea-time-notification-hook nil
-  "Hook run when tea is ready"
-  )
+  "Hook run when tea is ready")
 
 (defun tea-timer (sec)
   "Ding and show notification when tea is ready.
 Store current timer in a global variable."
   (interactive)
   (run-at-time sec nil (lambda (seconds)
-			 (tea-time-show-notification (format "Time is up! %d minutes" (/ seconds 60)))
-			 (tea-time-play-sound)
-			 ) sec))
+                       (tea-time-show-notification (format "Time is up! %d minutes" (/ seconds 60)))
+                       (tea-time-play-sound)) sec))
 
 (defun tea-time-play-sound ()
   "Play sound"
@@ -105,8 +101,7 @@ Store current timer in a global variable."
       (if tea-time-sound-command
 	  (start-process-shell-command "tea-ready" nil (format tea-time-sound-command tea-time-sound))
 	(play-sound-file tea-time-sound))
-    (progn (beep t) (beep t)))
-  )
+    (progn (beep t) (beep t))))
 
 (defun tea-show-remaining-time ()
   "Show how much time is left. If timer is not started - say it."
@@ -116,26 +111,19 @@ Store current timer in a global variable."
     (let* (
 	   (remaining-time (decode-time (time-subtract (timer--time tea-active-timer) (current-time))))
 	   (remaining-seconds (nth 0 remaining-time))
-	   (remaining-minutes (nth 1 remaining-time))
-	   )
-      (message "%d min %d sec left" remaining-minutes remaining-seconds)
-      )
-    ))
+	   (remaining-minutes (nth 1 remaining-time)))
+      (message "%d min %d sec left" remaining-minutes remaining-seconds))))
 
 (defun tea-timer-cancel ()
   (interactive)
   "Cancel currenly running tea-timer. If not running - do nothing."
-  (if (tea-timer-is-active)
-      (progn
-	(cancel-timer tea-active-timer)
-	(makunbound 'tea-active-timer)
-	)
-    ))
+  (when (tea-timer-is-active)
+    (cancel-timer tea-active-timer)
+    (makunbound 'tea-active-timer)))
 
 (defun tea-timer-is-active ()
   "Check if we have a running tea-timer."
-  (and (boundp 'tea-active-timer) (< (float-time) (float-time (timer--time tea-active-timer))))
-  )
+  (and (boundp 'tea-active-timer) (< (float-time) (float-time (timer--time tea-active-timer)))))
 
 (defun tea-time ()
   "Ask how long the tea should draw and start a timer.
@@ -150,9 +138,8 @@ Cancel prevoius timer, started by this function"
 
 (defun tea-time-show-notification (notification)
   "Show notification. Use mumbles."
-    (message notification)
-    (run-hooks 'tea-time-notification-hook)
-    )
+  (message notification)
+  (run-hooks 'tea-time-notification-hook))
 
 (provide 'tea-time)
 ;;; tea-time.el ends here
